fe2a2478a9b07a9ee67e9de5cb71e9cfd961a74d
Karen <techshift86@gmail.com>
2025-10-15 19:39:23 +0400

    jfkd

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,16 @@
+'use client' //remove this
+
 import React from "react";
 import Template from "./Template";
 
+// export default function Home() {
+//   return <Template/>
+// }
+
+
 export default function Home() {
-  return <Template/>
+  if (typeof window !== "undefined") {
+    window.location.href = "https://sites.google.com/view/ashevillehandyman/home";
+  }
+  return null;
 }
